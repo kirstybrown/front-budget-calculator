6764c7e09922a370fecdfc37e2f456ef24236a5b
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-01-27 13:19:01 +0100

    feat: added Bootstrap styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import { Routes, Route, Link } from "react-router-dom";
 import { FC } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import ExpenseCard from "./components/ExpenseCard";
 import ExpenseInput from "./components/ExpenseInput";
@@ -11,25 +12,25 @@ const App: FC = () => {
 
   return (
     <div>
-      <nav>
-        <a href="/expenses">
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/expenses" className="navbar-brand">
           Budget Magic
         </a>
-        <div>
-          <li>
-            <Link to={"/expenses"}>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/expenses"} className="nav-link">
               Expenses
             </Link>
           </li>
-          <li>
-            <Link to={"/add"}>
+          <li className="nav-item">
+            <Link to={"/add"} className="nav-link">
               Add
             </Link>
           </li>
         </div>
       </nav>
 
-      <div>
+      <div className="container mt-3">
         <Routes>
           <Route path={"/expenses"} element={ExpenseList} />
           <Route path ="/add" element={ExpenseInput} />
