0d03058e1bbab11ebca2d0a04b795e68bbeac873
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-01-27 11:13:56 +0100

    refactor: added nav bar with Links, and Switch object with Route to each Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,41 @@
-import { FC, ChangeEvent, useState } from "react";
+import { FC } from "react";
+import React, { Switch, Route, Link } from "react-router-dom";
 import './App.css';
-import Expense from "./components/expense.component";
-import {IExpense} from './Interfaces';
+import ExpenseCard from "./components/ExpenseCard";
+import ExpenseInput from "./components/ExpenseInput";
+import ExpenseList from "./components/ExpenseList";
+
 
 const App: FC = () => {
 
-  const [expense, setExpense] = useState<string>("");
-  const [amount, setAmount] = useState<number>(0);
-  const [category, setCategory] = useState<string>("");
-  const [expenseList, setExpenseList] = useState<IExpense[]>([]);
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
-    if (event.target.name === "expense") {
-      setExpense(event.target.value);
-    };
-    if (event.target.name === "amount") {
-      setAmount(Number(event.target.value));
-    };
-    if (event.target.name === "category") {
-      setCategory(event.target.value);
-    };
-  };
-
-  const addExpense = (): void => {
-    const newExpense = {expenseName: expense, expenseAmount: amount, expenseCategory: category};
-    setExpenseList([...expenseList, newExpense]);
-    setExpense("");
-    setAmount(0);
-    setCategory("");
-  }
-
-  const deleteExpense = (expenseToDelete: string) : void => {
-    setExpenseList(expenseList.filter((expense) => {
-      return expense.expenseName !== expenseToDelete;
-    }))
-  }
-
   return (
-    <div className="App">
-      <div className='header'>
-        <div className="input-container">
-          <input 
-            type="text" 
-            placeholder="Expense" 
-            name="expense" 
-            value={expense}
-            onChange={handleChange}
-          />
-          <input 
-            type="number" 
-            placeholder="Amount" 
-            name="amount"
-            value={amount}
-            onChange={handleChange}
-          />
-          <input 
-            type="text" 
-            placeholder="Category" 
-            name="category"
-            value={category}
-            onChange={handleChange}
-          />
+    <div>
+      <nav>
+        <a href="/expenses">
+          Budget Magic
+        </a>
+        <div>
+          <li>
+            <Link to={"/expenses"}>
+              Expenses
+            </Link>
+          </li>
+          <li>
+            <Link to={"/add"}>
+              Add
+            </Link>
+          </li>
         </div>
-        <button onClick={addExpense}>Add Expense</button>
+      </nav>
+
+      <div>
+        <Switch>
+          <Route exact path={["/", "/expenses"]} component={ExpenseList} />
+          <Route exact path="/add" component={ExpenseInput} />
+          <Route exact path="/expenses/:id" component={ExpenseCard} />
+        </Switch>
       </div>
-      <div className='expense-list'>
-        {expenseList.map((expense: IExpense, key: number) => {
-          return <Expense key={key} expense={expense} deleteExpense={deleteExpense}/>;
-        })}
-      </div>
+
     </div>
   );
 }
